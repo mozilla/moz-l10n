572f8894b34c91ecee322a3e15c3347eeba04777
Eemeli Aro <eemeli@mozilla.com>
2024-05-07 20:19:33 +0300

    Add iter_resources(), with built-in .l10n-ignore support

--- moz/l10n/resource/__init__.py
+++ moz/l10n/resource/__init__.py
@@ -1,5 +1,12 @@
 from .format import Format, detect_format
+from .iter_resources import iter_resources
 from .parse_resource import parse_resource
 from .serialize_resource import serialize_resource
 
-__all__ = ["Format", "detect_format", "parse_resource", "serialize_resource"]
+__all__ = [
+    "Format",
+    "detect_format",
+    "iter_resources",
+    "parse_resource",
+    "serialize_resource",
+]
