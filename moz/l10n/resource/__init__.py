2dc1121e9a3f7d6f027f2ff1a329c5c07c25c389
Eemeli Aro <eemeli@mozilla.com>
2024-05-20 03:49:05 +0300

    Add custom UnsupportedResource exception

--- moz/l10n/resource/__init__.py
+++ moz/l10n/resource/__init__.py
@@ -1,9 +1,10 @@
 from .format import Format, detect_format
-from .parse_resource import parse_resource
+from .parse_resource import UnsupportedResource, parse_resource
 from .serialize_resource import serialize_resource
 
 __all__ = [
     "Format",
+    "UnsupportedResource",
     "detect_format",
     "parse_resource",
     "serialize_resource",
