c05fdef8f5f4f570467aaf020cde43fcb267a025
Eemeli Aro <eemeli@mozilla.com>
2024-06-01 18:09:15 +0300

    Add l10n_extensions and bilingual_extensions sets to moz.l10n.resource
    Co-authored-by: Matjaž Horvat <matjaz.horvat@gmail.com>

--- moz/l10n/resource/__init__.py
+++ moz/l10n/resource/__init__.py
@@ -1,11 +1,13 @@
-from .format import Format, detect_format
+from .format import Format, bilingual_extensions, detect_format, l10n_extensions
 from .parse_resource import UnsupportedResource, parse_resource
 from .serialize_resource import serialize_resource
 
 __all__ = [
     "Format",
     "UnsupportedResource",
+    "bilingual_extensions",
     "detect_format",
+    "l10n_extensions",
     "parse_resource",
     "serialize_resource",
 ]
