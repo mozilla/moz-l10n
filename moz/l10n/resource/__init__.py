1999369851d86019c5e36c45bfc34b4f2846735f
Eemeli Aro <eemeli@mozilla.com>
2024-06-22 17:00:11 +0300

    Add moz.l10n.resource.l10n_equal comparator

--- moz/l10n/resource/__init__.py
+++ moz/l10n/resource/__init__.py
@@ -1,4 +1,5 @@
 from .format import Format, bilingual_extensions, detect_format, l10n_extensions
+from .l10n_equal import l10n_equal
 from .parse_resource import UnsupportedResource, parse_resource
 from .serialize_resource import serialize_resource
 
@@ -8,6 +9,7 @@ __all__ = [
     "bilingual_extensions",
     "detect_format",
     "l10n_extensions",
+    "l10n_equal",
     "parse_resource",
     "serialize_resource",
 ]
