c0b8f664250e7b195e8c28ce69aebcf7dfff49bf
Eemeli Aro <eemeli@mozilla.com>
2024-10-03 18:19:26 +0300

    build: Also build Fluent by iterating source, report ftl_missing count

--- moz/l10n/bin/build.py
+++ moz/l10n/bin/build.py
@@ -75,7 +75,7 @@ def cli() -> None:
     l10n_target: str = args.target
     locales: set[str] = set(args.locales)
 
-    # locale -> [ftl_filtered, l10n_fallback]
+    # locale -> [ftl_missing, src_fallback]
     msg_data: dict[str, list[int]] = defaultdict(lambda: [0, 0])
 
     paths = L10nConfigPaths(cfg_path)
@@ -110,12 +110,12 @@ def cli() -> None:
                 log.info(f"skip {rel_path}")
 
     log.info("----")
-    for locale, (ftl_missing, l10n_fallback) in sorted(
+    for locale, (ftl_missing, src_fallback) in sorted(
         msg_data.items(), key=lambda d: d[0]
     ):
         log.info(f"{locale}:")
-        log.info(f"  missing_ftl {ftl_missing:>6}")
-        log.info(f"  fallback    {l10n_fallback:>6}")
+        log.info(f"  ftl_missing  {ftl_missing:>6}")
+        log.info(f"  src_fallback {src_fallback:>6}")
 
 
 def write_target_file(
@@ -124,90 +124,53 @@ def write_target_file(
     l10n_path: str,
     tgt_path: str,
 ) -> int:
-    l10n_res = parse_resource(l10n_path) if exists(l10n_path) else None
-
-    if source_res.format == Format.fluent:
-        # Fluent uses per-message fallback at runtime, allowing resources to be incomplete.
-        # Therefore, build result by filtering out any content not in the source.
-        if l10n_res is None:
-            msg_delta = -sum(
-                sum(1 for entry in section.entries if isinstance(entry, Entry))
-                for section in source_res.sections
-            )
-            log.info(f"create empty {name} ({msg_delta})")
-            open(tgt_path, "a").close()
-            return msg_delta
-
-        source_map = _message_map(name, source_res)
-        msg_delta = 0
-        for section in l10n_res.sections:
-            msg_delta -= len(section.entries)
-            section.entries = [
-                entry
-                for entry in section.entries
-                if isinstance(entry, Entry) and section.id + entry.id in source_map
-            ]
-            msg_delta += len(section.entries)
-        msg = f"filter {name}"
-        log.info(f"{msg} ({msg_delta})" if msg_delta != 0 else msg)
-        with open(tgt_path, "w") as file:
-            for line in serialize_resource(l10n_res, trim_comments=True):
-                file.write(line)
-        return msg_delta
-
-    # For other formats, build result from source,
-    # using any localized messages that are available.
-    if l10n_res is None:
-        l10n_map = {}
-        l10n_res = Resource(source_res.format, [])
-    else:
-        l10n_map = _message_map(l10n_path, l10n_res)
+    if exists(l10n_path):
+        l10n_res = parse_resource(l10n_path)
+        l10n_map = {
+            section.id + entry.id: entry
+            for section in l10n_res.sections
+            for entry in section.entries
+            if isinstance(entry, Entry)
+        }
         l10n_res.sections = []
+    else:
+        l10n_res = Resource(source_res.format, [])
+        l10n_map = {}
+    # Fluent uses per-message fallback at runtime, allowing resources to be incomplete.
+    fill_from_source = source_res.format != Format.fluent
     msg_delta = 0
 
     def get_entry(
-        section_id: tuple[str, ...], source_entry: Entry[Message, str]
-    ) -> Entry[Message, str] | Comment:
+        section_id: tuple[str, ...], source_entry: Entry[Message, str] | Comment
+    ) -> Entry[Message, str] | Comment | None:
+        nonlocal msg_delta
+        if isinstance(source_entry, Comment):
+            return None
         id = section_id + source_entry.id
         if id in l10n_map:
             return l10n_map[id]
-        else:
-            nonlocal msg_delta
+        elif fill_from_source:
             msg_delta += 1
             return source_entry
+        else:
+            msg_delta -= 1
+            return None
 
     for section in source_res.sections:
         tgt_entries = [
-            get_entry(section.id, entry)
-            for entry in section.entries
-            if isinstance(entry, Entry)
+            entry
+            for entry_ in section.entries
+            if (entry := get_entry(section.id, entry_)) is not None
         ]
         l10n_res.sections.append(Section(section.id, tgt_entries))
-    msg = f"fill {name}"
-    log.info(f"{msg} (+{msg_delta})" if msg_delta != 0 else msg)
+
+    msg = f"merge {name}"
+    log.info(f"{msg} ({msg_delta:+d})" if msg_delta != 0 else msg)
     with open(tgt_path, "w") as file:
         for line in serialize_resource(l10n_res, trim_comments=True):
             file.write(line)
     return msg_delta
 
-
-def _message_map(
-    path: str,
-    resource: Resource[Message, str],
-) -> dict[tuple[str, ...], Entry[Message, str]]:
-    if path in _message_id_map_cache:
-        return _message_id_map_cache[path]
-    else:
-        id_map = _message_id_map_cache[path] = {
-            section.id + entry.id: entry
-            for section in resource.sections
-            for entry in section.entries
-            if isinstance(entry, Entry)
-        }
-        return id_map
-
-
-_message_id_map_cache: dict[str, dict[tuple[str, ...], Entry[Message, str]]] = {}
 
 if __name__ == "__main__":
     cli()
