4eeb4437c1ffc2b7bfc57e009e52071573ed08e4
Eemeli Aro <eemeli@mozilla.com>
2024-10-03 13:47:36 +0300

    Check for ImportError before LxmlError, as the latter depends on the import (#23)
    * ci: Test without [xml] extra
    * tests: Skip android & xliff tests if lxml not available

--- tests/resource/test_detect_format.py
+++ tests/resource/test_detect_format.py
@@ -14,32 +14,44 @@
 
 from __future__ import annotations
 
+from importlib.util import find_spec
 from importlib_resources import files
-from unittest import TestCase
+from unittest import TestCase, skipIf
 
 from moz.l10n.resource import Format, detect_format
 
+no_xml = find_spec("lxml") is None
+
 
 class TestDetectFormat(TestCase):
-    def test_data_files(self):
+    def test_common_data_files(self):
         data = {
             "accounts.dtd": Format.dtd,
-            "angular.xliff": Format.xliff,
             "bug121341.properties": Format.properties,
             "defines.inc": Format.inc,
             "demo.ftl": Format.fluent,
             "foo.po": Format.po,
+            "messages.json": Format.webext,
+            "test.properties": Format.properties,
+        }
+        for file, exp_format in data.items():
+            source = files("tests.resource.data").joinpath(file).read_bytes()
+            assert detect_format(file, source) == exp_format
+
+    @skipIf(no_xml, "Requires [xml] extra")
+    def test_xml_data_files(self):
+        data = {
+            "angular.xliff": Format.xliff,
             "hello.xliff": Format.xliff,
             "icu-docs.xliff": Format.xliff,
-            "messages.json": Format.webext,
             "strings.xml": Format.android,
-            "test.properties": Format.properties,
             "xcode.xliff": Format.xliff,
         }
         for file, exp_format in data.items():
             source = files("tests.resource.data").joinpath(file).read_bytes()
             assert detect_format(file, source) == exp_format
 
+    @skipIf(no_xml, "Requires [xml] extra")
     def test_xliff_source(self):
         for file in ("angular.xliff", "hello.xliff", "icu-docs.xliff", "xcode.xliff"):
             source = files("tests.resource.data").joinpath(file).read_bytes()
