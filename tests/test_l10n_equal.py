937e19029bf06eb7a81d1c33faf76659c9a3b9b5
Eemeli Aro <eemeli@mozilla.com>
2024-11-21 01:23:01 +0200

    l10n-compare: Conflate anonymous sections together (#30)

--- tests/test_l10n_equal.py
+++ tests/test_l10n_equal.py
@@ -93,3 +93,16 @@ class TestL10nEqual(TestCase):
         a = Resource(None, [Section((), [Entry(("foo",), "Foo", meta=am)])])
         b = Resource(None, [Section((), [Entry(("foo",), "Foo", meta=bm)])])
         assert l10n_equal(a, b)
+
+    def test_empty_sections(self):
+        a = Resource(
+            None, [Section((), [Entry(("foo",), "Foo"), Entry(("bar",), "Bar")])]
+        )
+        b = Resource(
+            None,
+            [
+                Section((), [Entry(("bar",), "Bar")]),
+                Section((), [Entry(("foo",), "Foo")]),
+            ],
+        )
+        assert l10n_equal(a, b)
