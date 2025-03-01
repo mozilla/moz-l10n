2b5236caaaf14ace9712c1cfcd9511827f1317c7
Eemeli Aro <eemeli@mozilla.com>
2025-03-01 14:17:49 +0200

    webext: Add and expose webext_parse_message

--- python/moz/l10n/formats/webext/__init__.py
+++ python/moz/l10n/formats/webext/__init__.py
@@ -1,4 +1,4 @@
-from .parse import webext_parse
+from .parse import webext_parse, webext_parse_message
 from .serialize import webext_serialize
 
-__all__ = ["webext_parse", "webext_serialize"]
+__all__ = ["webext_parse", "webext_parse_message", "webext_serialize"]
