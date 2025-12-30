7b4938d4f6d8761404f3482644ca26682e44736a
Eemeli Aro <eemeli@mozilla.com>
2025-12-30 20:04:37 +0200

    test: Store example migration as separate file fixture

--- python/tests/test_migrate.py
+++ python/tests/test_migrate.py
@@ -14,6 +14,7 @@
 
 from __future__ import annotations
 
+from importlib_resources import files
 from os.path import isfile, join
 from tempfile import TemporaryDirectory
 from textwrap import dedent
@@ -249,111 +250,6 @@ class TestMigrate(TestCase):
                     assert file.read() == "[Strings]\n" + "d1=C\n"
 
     def test_firefox_plural_properties(self):
-        migration = dedent(
-            """\
-            import re
-            from typing import cast
-
-            from moz.l10n.migrate import Migrate
-            from moz.l10n.migrate.utils import MigrationContext, get_entry
-            from moz.l10n.model import (
-                CatchallKey,
-                Expression,
-                PatternMessage,
-                SelectMessage,
-                VariableRef,
-            )
-
-
-            def parse_pattern(src: str):
-                pos = 0
-                for m in re.finditer(r"#1|#2|%d", src):
-                    start = m.start()
-                    if start > pos:
-                        yield src[pos:start]
-                    yield Expression(VariableRef("n" if m[0] == "#1" else "x"))
-                    pos = m.end()
-                if pos < len(src):
-                    yield src[pos:]
-
-
-            def get_key(locale: str, idx: int):
-                if locale in {"ltg", "lv"}:
-                    categories = ["zero", "one", CatchallKey("other")]
-                elif locale in {"bs", "hr", "lt", "ro", "sr"}:
-                    categories = ["one", "few", CatchallKey("other")]
-                elif locale in {"be", "cs", "pl", "ru", "sk", "szl", "uk"}:
-                    categories = ["one", "few", CatchallKey("many")]
-                elif locale in {"dsb", "gd", "hsb", "sl"}:
-                    categories = ["one", "two", "few", CatchallKey("other")]
-                elif locale in {"br", "ga"}:
-                    categories = ["one", "two", "few", "many", CatchallKey("other")]
-                elif locale == "ar":
-                    categories = ["one", "two", "few", "many", CatchallKey("other"), "zero"]
-                elif locale == "cy":
-                    categories = ["zero", "one", "two", "few", "many", CatchallKey("other")]
-                else:
-                    categories = ["one", CatchallKey("other")]
-                return categories[idx if idx < len(categories) else -1]
-
-
-            plural_categories = ["zero", "one", "two", "few", "many", "other"]
-
-
-            def plural(ref_path: str, id: str):
-                def plural_(_, ctx: MigrationContext):
-                    res = ctx.get_resource(ref_path)
-                    if res is None:
-                        return None
-                    entry = get_entry(res, id)
-                    if entry is None:
-                        return None
-                    pattern = cast(PatternMessage, entry.value).pattern
-                    assert len(pattern) == 1
-                    assert isinstance(pattern[0], str)
-                    parts = pattern[0].split(";")
-
-                    if len(parts) > 1:
-                        var_list = [
-                            (get_key(ctx.locale, idx), part) for idx, part in enumerate(parts)
-                        ]
-                        var_list.sort(
-                            key=lambda v: plural_categories.index(k)
-                            if isinstance(k := v[0], str) and k in plural_categories
-                            else 6
-                        )
-                        entry.value = SelectMessage(
-                            {"n": Expression(VariableRef("n"), "number")},
-                            (VariableRef("n"),),
-                            {(key,): list(parse_pattern(part)) for key, part in var_list},
-                        )
-
-                    entry.comment = re.sub(
-                        r"LOCALIZATION NOTE.*?:|Semi-colon list of plural forms.|See: http.*?/Localization_and_Plurals",
-                        "",
-                        entry.comment,
-                    ).strip()
-
-                    return (entry, {id})
-
-                return plural_
-
-
-            Migrate(
-                {
-                    "debugger.ftl": {
-                        "source-search-results-summary": plural(
-                            "debugger.properties", "sourceSearch.resultsSummary2"
-                        ),
-                        "editor-search-results": plural(
-                            "debugger.properties", "editor.searchResults1"
-                        ),
-                    }
-                }
-            )
-            """
-        )
-
         tree: Tree = {
             "en-US": {"debugger.ftl": "", "debugger.properties": ""},
             "fr": {
@@ -387,12 +283,14 @@ class TestMigrate(TestCase):
                     editor.searchResults1 = نتيجة واحدة;%d من أصل نتيجتين;%d من أصل #1 نتائج;%d من أصل #1 نتيجة;%d من أصل #1 نتيجة;لا نتائج
                 """)
             },
-            "migration.py": migration,
         }
         with TemporaryDirectory() as root:
             build_file_tree(root, tree)
 
-            cli(["--root", root, join(root, "migration.py")])
+            migration_path = files("tests.fixtures").joinpath(
+                "migrate_pluralform_properties_to_fluent.py"
+            )
+            cli(["--root", root, str(migration_path)])
             with open(join(root, "fr", "debugger.ftl"), encoding="utf-8") as file:
                 assert file.read() == dedent("""\
                     # Shows a summary of the number of matches for autocomplete
